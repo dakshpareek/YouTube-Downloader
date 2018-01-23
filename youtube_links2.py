99f735b84d4f4cb7ce71336d2948a4ab49ee8579
Daksh Pareek <dakshpareek7@gmail.com>
2018-01-23 11:11:27 +0530

    Update youtube_links2.py

--- youtube_links2.py
+++ youtube_links2.py
@@ -3,9 +3,20 @@ import requests
 from bs4 import BeautifulSoup
 from pytube import YouTube
 
-ch=input("1. Link 2. Playlist")
+
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
+
+ch=input("1. Link 2. Playlist :-->  ")
+#ch2=input("1. Video 2. Audio :-->> ")
+#tipe=False if ch2=="1" else True
+tipe=False
 if ch==2:
-	url1=raw_input("Enter Playlist Link")
+	url1=raw_input("Enter Playlist Link: ")
 	#url1="https://www.youtube.com/playlist?list=PLFACC72B70062EB2D"
 
 	r = requests.get(url1)
@@ -29,21 +40,32 @@ if ch==2:
 	for every_video in all_video_links_in_playlist[20:]:
 	#for every_video in all_video_links_in_playlist:
 		yt=YouTube(every_video)
-		print "Video Title :",yt.title
-		stream=yt.streams.filter(file_extension='mp4').all()
+		print "Title :",yt.title
+		stream=yt.streams.filter(only_audio=tipe).all()
 		print "Downloading.."
 		stream[0].download()
-		print "Download Complete"
-	print "Task Completed"
-	
+		print "--Download Complete--"
+	print "*** Task Completed ***"
+
 elif ch==1:
-	url1=raw_input("Enter YouTube Link")
+	url1=raw_input("Enter YouTube Link: ")
 	yt=YouTube(url1)
-	print "Video Title :",yt.title
-	stream=yt.streams.filter(file_extension='mp4').all()
-	print "Downloading.."
-	stream[0].download()
-	print "Download Complete"
+	print "Title : ",yt.title
+	stream=yt.streams.filter(only_audio=tipe).all()
+	#stream[0].download()
+	si=stream[0].filesize
+	#print stream
+	si=sizeof_fmt(si) 
+	print "File Size: ",si
+	ch=raw_input("Download??  y / n ")
+	if ch=='y' or ch=='Y':
+		print " Downloading.. "
+		stream[0].download()
+		print "--- Download Complete ---"
+	else:
+		print "Exit"
+
+
 
 
 
